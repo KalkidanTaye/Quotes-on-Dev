f1eb501345a7dc19bae7f784f886684bb7d98b13
Kalkidan Alemu <kali.taye@gmail.com>
2019-09-18 02:16:41 -0400

    Style: Single.php

--- single.php
+++ single.php
@@ -9,7 +9,8 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+		<div class="left-quote"><i class="fas fa-quote-left"></i></div>
+		<div class="single-content">
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
@@ -17,7 +18,8 @@ get_header(); ?>
 			<?php the_post_navigation(); ?>
 
 		<?php endwhile; // End of the loop. ?>
-
+</div>
+		<div class="right-quote"> <i class="fas fa-quote-right"></i></div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
