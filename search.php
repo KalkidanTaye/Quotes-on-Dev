a2178b26ca5bc9a956cf9f6e24b460e679a0e569
Kalkidan Alemu <kali.taye@gmail.com>
2019-09-18 03:21:46 -0400

    Fix: 404 margin and width

--- search.php
+++ search.php
@@ -31,9 +31,9 @@ get_header(); ?>
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
-		</div>
 		
-		<div class="right-quote"> <i class="fas fa-quote-right"></i></div>
+		
+	<div class="right-quote"> <i class="fas fa-quote-right"></i></div>
 		      </main><!-- #main -->
 	      </div><!-- #primary -->
 
